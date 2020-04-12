726c169e0fc521b6f62a245664b3ac355e1c6e55
Dan Smith <dan@getabetterpic.com>
2020-04-12 16:53:55 -0400

    Setup initial styles and shell component

--- apps/fly-my-rockets/src/app/app.component.ts
+++ apps/fly-my-rockets/src/app/app.component.ts
@@ -1,13 +1,8 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Message } from '@fly-my-rockets/api-interfaces';
 
 @Component({
-  selector: 'fly-my-rockets-root',
+  selector: 'fmr-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  hello$ = this.http.get<Message>('/api/hello');
-  constructor(private http: HttpClient) {}
-}
+export class AppComponent {}
--- apps/fly-my-rockets/src/app/app.module.ts
+++ apps/fly-my-rockets/src/app/app.module.ts
@@ -5,9 +5,19 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { SharedModule } from './shared/shared.module';
+import { HomePageComponent } from './home-page/home-page.component';
+import { AppRoutingModule } from './app-routing.module';
+
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, BrowserAnimationsModule],
+  declarations: [AppComponent, HomePageComponent],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    SharedModule,
+    AppRoutingModule,
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
