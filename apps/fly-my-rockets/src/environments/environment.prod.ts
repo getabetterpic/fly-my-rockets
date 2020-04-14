3daab351010f4c2a6039a57add66a2ee2369f6ca
Dan Smith <dan@getabetterpic.com>
2020-04-14 07:52:42 -0400

    Add prod firebase config

--- apps/fly-my-rockets/src/environments/environment.prod.ts
+++ apps/fly-my-rockets/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyC-O5Oy2Zgqq_am2HrasNSxb_0Grv4QeJo",
+    authDomain: "fly-my-rockets.firebaseapp.com",
+    databaseURL: "https://fly-my-rockets.firebaseio.com",
+    projectId: "fly-my-rockets",
+    storageBucket: "fly-my-rockets.appspot.com",
+    messagingSenderId: "42374420864",
+    appId: "1:42374420864:web:a01f160df1e5c462c14965",
+    measurementId: "G-3N5FH2QGDJ"
+  }
 };
