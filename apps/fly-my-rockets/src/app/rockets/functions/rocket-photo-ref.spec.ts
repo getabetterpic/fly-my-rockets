131c8cb307115278149bf0420788699e4bb6b51a
Dan Smith <dan@getabetterpic.com>
2020-04-19 16:03:52 -0400

    Transform an image ref into its equivalent thumbnail size

---
+++ apps/fly-my-rockets/src/app/rockets/functions/rocket-photo-ref.spec.ts
@@ -0,0 +1,23 @@
+import { rocketPhotoRef, ThumbnailSizes } from './rocket-photo-ref';
+
+describe('rocketPhotoRef', () => {
+  let originalRef;
+
+  beforeEach(() => {
+    originalRef = 'asdf1234/images/rockets/IMG_1234.jpg';
+  });
+
+  describe('when getting the small ref', () => {
+    it('returns the correct string', () => {
+      const ref = rocketPhotoRef(originalRef, ThumbnailSizes.Small);
+      expect(ref).toEqual('asdf1234/images/rockets/thumbnails/IMG_1234_50x50.jpg');
+    });
+  });
+
+  describe('when getting the medium ref', () => {
+    it('returns the correct string', () => {
+      const ref = rocketPhotoRef(originalRef, ThumbnailSizes.Medium);
+      expect(ref).toEqual('asdf1234/images/rockets/thumbnails/IMG_1234_400x600.jpg');
+    });
+  });
+});
