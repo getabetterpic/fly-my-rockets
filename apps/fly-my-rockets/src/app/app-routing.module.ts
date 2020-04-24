a1f76804ab25a499ba966b18cef57b619cdf9b3f
Dan Smith <dan@getabetterpic.com>
2020-04-24 09:36:52 -0400

    Redirect to /rockets after signing in

--- apps/fly-my-rockets/src/app/app-routing.module.ts
+++ apps/fly-my-rockets/src/app/app-routing.module.ts
@@ -1,26 +1,37 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
-import { AuthGuard } from './user/auth.guard';
+import {
+  AngularFireAuthGuard,
+  redirectLoggedInTo,
+  redirectUnauthorizedTo
+} from '@angular/fire/auth-guard';
+
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['/login']);
+const redirectLoggedInToRockets = () => redirectLoggedInTo(['/rockets']);
 
 const routes: Routes = [
   {
-    path: '', component: HomePageComponent
+    path: '',
+    component: HomePageComponent
   },
   {
-    path: 'login', loadChildren: () => import('./user/user.module').then(m => m.UserModule)
+    path: 'login',
+    canActivate: [AngularFireAuthGuard],
+    data: { authGuardPipe: redirectLoggedInToRockets },
+    loadChildren: () => import('./user/user.module').then(m => m.UserModule)
   },
   {
     path: 'rockets',
-    loadChildren: () => import('./rockets/rockets.module').then(m => m.RocketsModule),
-    canActivate: [AuthGuard]
+    loadChildren: () =>
+      import('./rockets/rockets.module').then(m => m.RocketsModule),
+    canActivate: [AngularFireAuthGuard],
+    data: { authGuardPipe: redirectUnauthorizedToLogin }
   }
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes)
-  ],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
