8dd0d4b683a648e45c3f43a6ec58c36ab091b3c8
Dan Smith <dan@getabetterpic.com>
2020-04-25 13:38:12 -0400

    Set up config for staging deploy

---
+++ apps/fly-my-rockets/src/environments/environment.staging.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCeTtm6lS2V7CUgAEznap1v997NlB83MGU',
+    authDomain: 'fly-my-rockets---staging.firebaseapp.com',
+    databaseURL: 'https://fly-my-rockets---staging.firebaseio.com',
+    projectId: 'fly-my-rockets---staging',
+    storageBucket: 'fly-my-rockets---staging.appspot.com',
+    messagingSenderId: '347228022343',
+    appId: '1:347228022343:web:c2fcca095949c48f00b362',
+    measurementId: 'G-RH0LSW5FM1'
+  }
+};
